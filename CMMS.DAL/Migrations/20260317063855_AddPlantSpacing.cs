3be0363c8d7e80d55adcd91b2a1d3b9c05a0aec9
Thuannt0403 <thaithuan4323123123@gmail.com>
2026-03-17 13:50:13 +0700

    migration plantspacing to crop and add response

---
+++ CMMS.DAL/Migrations/20260317063855_AddPlantSpacing.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMMS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlantSpacing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "plant_spacing",
+                schema: "public",
+                table: "crops",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "plant_spacing",
+                schema: "public",
+                table: "crops");
+        }
+    }
+}
