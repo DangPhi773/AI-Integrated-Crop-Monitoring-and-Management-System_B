f10ca98e7fdd8793405670a0b353dcb0409346cb
Thuannt0403 <thaithuan4323123123@gmail.com>
2026-03-23 11:57:43 +0700

    manage recommendation of specialist

---
+++ CMMS.BLL/Interfaces/IRecommendationService.cs
@@ -0,0 +1,19 @@
+﻿using CMMS.DAL.DTOs.Auth;
+using CMMS.DAL.DTOs.Recommendation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMMS.BLL.Interfaces
+{
+    public interface IRecommendationService
+    {
+        Task<ApiResponse<IEnumerable<RecommendationResponse>>> GetListAsync();
+        Task<ApiResponse<RecommendationResponse>> GetDetailAsync(Guid id);
+        Task<ApiResponse<string>> CreateAsync(RecommendationRequest request);
+        Task<ApiResponse<string>> UpdateAsync(Guid id, RecommendationRequest request);
+        Task<ApiResponse<string>> DeleteAsync(Guid id);
+    }
+}
---
+++ CMMS.DAL/DTOs/Recommendation/RecommendationDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMMS.DAL.DTOs.Recommendation
+{
+    public class RecommendationRequest
+    {
+        public Guid? SeasonId { get; set; }
+        public Guid? PestDetectionId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+    }
+
+    public class RecommendationResponse
+    {
+        public Guid RecommendationId { get; set; }
+        public Guid? SeasonId { get; set; }
+        public Guid? PestDetectionId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+
+        public string? PestLabel { get; set; }      
+        public string? PestSeverity { get; set; }   
+    }
+}
