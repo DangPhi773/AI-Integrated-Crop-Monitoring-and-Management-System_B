bf9ab1fe4b07eb7d8ccef027c4dd79db3eb4bdd2
Thuannt0403 <thaithuan4323123123@gmail.com>
2026-03-23 22:27:39 +0700

    Refactor task and task detail in db

---
+++ CMMS.DAL/Migrations/20260323152342_RefactorTaskAndDetailStructure_V2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMMS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorTaskAndDetailStructure_V2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "tasks_assigned_to_worker_id_fkey", schema: "public", table: "tasks");
+            migrationBuilder.DropForeignKey(name: "tasks_season_id_fkey", schema: "public", table: "tasks");
+
+            migrationBuilder.Sql("DROP INDEX IF EXISTS public.\"IX_tasks_season_id\";");
+            migrationBuilder.Sql("DROP INDEX IF EXISTS public.\"IX_tasks_assigned_to_worker_id\";");
+
+            migrationBuilder.DropColumn(name: "assigned_to_worker_id", schema: "public", table: "tasks");
+            migrationBuilder.DropColumn(name: "season_id", schema: "public", table: "tasks");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "assigned_to_worker_id",
+                schema: "public",
+                table: "task_detail",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_task_detail_assigned_to_worker_id",
+                schema: "public",
+                table: "task_detail",
+                column: "assigned_to_worker_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "task_detail_assigned_to_worker_id_fkey",
+                schema: "public",
+                table: "task_detail",
+                column: "assigned_to_worker_id",
+                principalSchema: "public",
+                principalTable: "users",
+                principalColumn: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
