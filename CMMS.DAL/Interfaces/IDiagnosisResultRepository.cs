f9a3f57e1c030d09b384711d10087ab0a7eaccb4
DangPhi773 <dangphi20102017@gmail.com>
2026-04-11 22:49:51 +0700

    feat(Diagnosis_Result): implement Get methods for view diagnosis report

--- CMMS.DAL/Interfaces/IDiagnosisResultRepository.cs
+++ CMMS.DAL/Interfaces/IDiagnosisResultRepository.cs
@@ -4,6 +4,9 @@ namespace CMMS.DAL.Interfaces
 {
     public interface IDiagnosisResultRepository
     {
+        Task<List<DiagnosisResult>> GetAllWithDetailsAsync();
+        Task<DiagnosisResult?> GetByIdWithDetailsAsync(Guid id);
+        Task<List<DiagnosisResult>> GetByReportIdAsync(Guid reportId);
         System.Threading.Tasks.Task AddAsync(DiagnosisResult entity);
         Task<bool> SaveChangesAsync();
     }
