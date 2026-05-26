1b54ddbc71e05bea8eb075692bd34a7ed4f51fb1
DangPhi773 <dangphi20102017@gmail.com>
2026-05-26 20:36:51 +0700

    fix(payment): change to new template url for QR

--- CMMS.BLL/Helpers/VietQrHelper.cs
+++ CMMS.BLL/Helpers/VietQrHelper.cs
@@ -6,7 +6,7 @@ namespace CMMS.BLL.Helpers;
 public static class VietQrHelper
 {
     private const string BaseUrl = "https://img.vietqr.io/image";
-    private const string Template = "compact2";
+    private const string Template = "compact";
 
     public static string? BuildImageUrl(
         string? bankBin,
@@ -20,8 +20,8 @@ public static class VietQrHelper
 
         var sb = new StringBuilder();
         sb.Append(BaseUrl).Append('/')
-          .Append(Uri.EscapeDataString(bankBin)).Append('/')
-          .Append(Uri.EscapeDataString(bankAccount)).Append('/')
+          .Append(Uri.EscapeDataString(bankBin)).Append('-')
+          .Append(Uri.EscapeDataString(bankAccount)).Append('-')
           .Append(Template).Append(".png");
 
         var query = new List<string>
@@ -30,12 +30,26 @@ public static class VietQrHelper
         };
 
         if (!string.IsNullOrWhiteSpace(addInfo))
-            query.Add("addInfo=" + Uri.EscapeDataString(addInfo));
+            query.Add("addInfo=" + Uri.EscapeDataString(RemoveDiacritics(addInfo)));
 
         if (!string.IsNullOrWhiteSpace(accountHolder))
-            query.Add("accountName=" + Uri.EscapeDataString(accountHolder));
+            query.Add("accountName=" + Uri.EscapeDataString(RemoveDiacritics(accountHolder)));
 
         sb.Append('?').Append(string.Join('&', query));
         return sb.ToString();
+    }
+
+    private static string RemoveDiacritics(string text)
+    {
+        var normalized = text.Normalize(NormalizationForm.FormD);
+        var sb = new StringBuilder(normalized.Length);
+        foreach (var c in normalized)
+        {
+            var category = System.Globalization.CharUnicodeInfo.GetUnicodeCategory(c);
+            if (category != System.Globalization.UnicodeCategory.NonSpacingMark)
+                sb.Append(c);
+        }
+        return sb.ToString().Normalize(NormalizationForm.FormC)
+            .Replace('Đ', 'D').Replace('đ', 'd');
     }
 }
